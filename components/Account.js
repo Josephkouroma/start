df8d052ac649622906b87d44c61dda1192dd5f29
Joseph Kouroma <jkouroma@gmail.com>
2022-05-04 12:16:50 +0100

    passing data into react componrnts

--- components/Account.js
+++ components/Account.js
@@ -1,16 +1,14 @@
 import EthName from './EthName'
 
-const Account = function () {
+const Account = function ({ account }) {
   // TODO!!!
-  // if already logged in, it should show 
+  // if already logged in, it should show
   // the EthName component with the correct address
   // if not logged in, show a connect button
   // that when its clicked, will prompt us to login
   // and store the info on the page
 
-  return (
-    <button>Connect</button>
-  )
-}
+  return <span>{account}</span>;
+};
 
 export default Account;
--- pages/index.js
+++ pages/index.js
@@ -22,56 +22,73 @@ export default function Home() {
   // 4. make the user name look good
   // 5. let the user post their own reply
 
+  let account = '0x1234';
+
   return (
     <main>
       <header>
         <h1>Potstop</h1>
 
         <form>
-          <input type="text" placeholder="Search" />
+          <input type='text' placeholder='Search' />
         </form>
 
-        <button>Connect</button>
+        <Account account={account} />
       </header>
 
-      <section className="question">
-        <div className="main">
+      <section className='question'>
+        <div className='main'>
           <h3>Feedback forum</h3>
           <h2>Looking for feedback as a beginner</h2>
-          <p>Hey everyone, I&apos;m a new potter, just 4 weeks into my journey, and I&apos;m looking to get some feedback on what I&apos;ve made so far. I&apos;m particularly interested in how to make rustic looking bowls and pots, and I&apos;d love to know what the best tools to use would be!</p>
+          <p>
+            Hey everyone, I&apos;m a new potter, just 4 weeks into my journey,
+            and I&apos;m looking to get some feedback on what I&apos;ve made so
+            far. I&apos;m particularly interested in how to make rustic looking
+            bowls and pots, and I&apos;d love to know what the best tools to use
+            would be!
+          </p>
 
-          <div className="slides">
-            <Image src="/image-1.jpg" width="600" height="800" />
-            <Image src="/image-2.jpg" width="600" height="800" />
-            <Image src="/image-3.jpg" width="600" height="800" />
-            <Image src="/image-4.jpg" width="600" height="800" />
+          <div className='slides'>
+            <Image src='/image-1.jpg' width='600' height='800' />
+            <Image src='/image-2.jpg' width='600' height='800' />
+            <Image src='/image-3.jpg' width='600' height='800' />
+            <Image src='/image-4.jpg' width='600' height='800' />
           </div>
         </div>
-        <div className="meta">
-          
+        <div className='meta'>
           {/* EthName */}
-          <div className="eth-name">
-            <img src="https://ipfs.io/ipfs/QmbctVN8tPaDLiLysVDwThf7JTJhMejbSypZ4a3v5H2G3a" alt="Avatar of riklomas.eth" />
-            <div className="name">
-              <span className="primary">riklomas.eth</span>
-              <span className="secondary">0xb25bf3...aaf4</span>
+          <div className='eth-name'>
+            <img
+              src='https://ipfs.io/ipfs/QmbctVN8tPaDLiLysVDwThf7JTJhMejbSypZ4a3v5H2G3a'
+              alt='Avatar of riklomas.eth'
+            />
+            <div className='name'>
+              <span className='primary'>riklomas.eth</span>
+              <span className='secondary'>0xb25bf3...aaf4</span>
             </div>
           </div>
           {/* end EthName */}
-
         </div>
       </section>
 
-      <section className="answers">
-        <div className="loading">Loading answers...</div>
+      <section className='answers'>
+        <div className='loading'>Loading answers...</div>
       </section>
 
       <Head>
-        <title>Looking for feedback as a beginner – Feedback forum – Potstop </title>
-        <meta property="og:title" content="Looking for feedback as a beginner on Potstop" />
-        <meta property="og:description" content="This is a project on the SuperHi Crypto + Web3 for Creatives course" />
-        <meta property="og:image" content="/social.png" />
+        <title>
+          Looking for feedback as a beginner – Feedback forum – Potstop{' '}
+        </title>
+        <meta
+          property='og:title'
+          content='Looking for feedback as a beginner on Potstop'
+        />
+        <meta
+          property='og:description'
+          content='This is a project on the SuperHi Crypto + Web3 for Creatives course'
+        />
+        <meta property='og:image' content='/social.png' />
       </Head>
     </main>
-  )
+  );
 }
