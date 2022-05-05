88a5b10474fecad33afec223f38befcbbc8c0315
Joseph Kouroma <jkouroma@gmail.com>
2022-05-05 17:09:29 +0100

    adding ENS avatars

--- components/EthName.js
+++ components/EthName.js
@@ -10,10 +10,8 @@ const ens = new ENS({
 });
 
 const EnsName = function ({ address }) {
-  // TODO!
-  // check for ENS domain
-  // get image if it has one
   const [name, setName] = useState();
+  const [avatar, setAvatar] = useState();
 
   useEffect(
     async function () {
@@ -25,12 +23,25 @@ const EnsName = function ({ address }) {
     [address]
   );
 
+  useEffect(
+    async function () {
+      if (name) {
+        const a = await ens.name(name).getText('avatar');
+
+        if (a) {
+          setAvatar(a);
+        }
+      }
+    },
+    [name]
+  );
+
   let formattedAddress = address.substr(0, 8) + '...' + address.substr(-4);
 
   let icon = <Jazzicon diameter={32} seed={jsNumberForAddress(address)} />;
   return (
     <div className='eth-name'>
-      <div className='icon'>{icon}</div>
+      <div className='icon'>{avatar ? avatar : icon}</div>
 
       <div className='name'>
         <span className='primary'>
